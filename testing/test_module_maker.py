d231e537eb6a76f29e05994c97cc239c3165e7be
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-10-04 00:16:34 +0200

    resuffle module metadata composition logic

--- testing/test_module_maker.py
+++ testing/test_module_maker.py
@@ -7,10 +7,12 @@ def test_update_script():
         pass
 
     # Genscript Metadata
+    ending
     """)
     result = update_script(str(source), dict(version='1.3'))
     print result
     assert "\nversion = '1.3'\n" in result
+    assert 'ending' in result
 
 def test_update_fails_for_missing():
     py.test.raises(ValueError, update_script, 'test_line\n', {})
